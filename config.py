bbad34adac09f0b25b7c7fab653f176189d3c679
Sanstar12 <94879410+Sanstar12@users.noreply.github.com>
2025-06-04 09:42:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "25214632"
+api_hash = "85f9204d557b13f5b336bd67dc073a46"
+bot_token = "8123711815:AAH9Dxnyn86kHGm1Ruv97kp4DRYNjT9Rqw8"
+auth_users = [6618712970]
